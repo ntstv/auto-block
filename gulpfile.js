1fb6a4f7aedc775ff518574c46520773fce9454d
Roman Shamyan <shamyan.roman@gmail.com>
2016-02-06 12:31:06 +0300

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp"),
+    sourcemaps = require("gulp-sourcemaps"),
+    babel = require("gulp-babel"),
+    concat = require("gulp-concat");
+
+gulp.task("default", function () {
+  return gulp.src("src/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat("all.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("dist"));
+});
+
--- src/index.js
+++ src/index.js
@@ -10,5 +10,7 @@
 
     }
 
+    console.log('Hello world');
+
 
 })();
