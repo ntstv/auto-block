a84c442c3a24840d30304c2bc8bd37d70edcb82b
Roman Shamyan <shamyan.roman@gmail.com>
2016-02-06 13:54:36 +0000

    removed concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
-var gulp = require("gulp"),
-    sourcemaps = require("gulp-sourcemaps"),
-    babel = require("gulp-babel"),
-    concat = require("gulp-concat");
+var gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    babel = require('gulp-babel');
 
-gulp.task("default", function () {
-  return gulp.src("src/**/*.js")
+gulp.task('default', function () {
+  return gulp
+    .src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel())
-    //.pipe(concat("all.js"))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist"));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['src/**/*.js', 'templates/**/*.nj'], ['default']);
+    gulp.watch('src/**/*.js', ['default']);
 });
